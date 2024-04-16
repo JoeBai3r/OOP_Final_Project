d6084c819b6123855141a7657e815b01a1a628c5
Joseph <confortatus@gmail.com>
2024-04-16 11:44:17 -0400

    added the basic classes needed with some of the required functions, may no be complete

--- final.java
+++ final.java
@@ -1,2 +1,44 @@
-//Group Members: Joseph Baier
-// Yoan Molina
+//Group Members: Joseph Baier, Yoan Molina
+
+import java.util.Scanner;
+
+public class DriverClass {
+    public static void main(String[] args) {
+
+    }
+}
+
+public class MainMenu {
+
+    public static void print() {
+
+    }
+}
+
+public abstract class Student {
+
+    abstract public void printInvoice() {
+
+    }
+
+}
+
+public abstract class GraduateStudent extends Student {
+
+}
+
+public class UndergraduateStudent extends Student {
+
+}
+
+public class MsStudent extends GraduateStudent {
+
+}
+
+public class PhdStudent extends GraduateStudent {
+
+}
+
+
+
+
