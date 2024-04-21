0e852da48a0d8d0fc3fecd056c6b0a29969409ef
dhablichcastillo08 <da778455@ucf.edu>
2024-04-21 13:08:17 -0400

    added setters and getters for Student

--- ProjectDriver.java
+++ ProjectDriver.java
@@ -51,6 +51,19 @@ public abstract class Student {
     private String name;
     private String id;
 
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public Student ( String name , String id) {
         this.name = name;
         this.id = id;
